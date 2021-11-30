4d6259d6f9ba1c660f27ca6cc8b073e4d32214f4
Ravi K <ravi.maurya@nickelfox.com>
2021-11-30 17:22:24 +0530

    fix: fix public layout and routing

--- src/layout/publicLayout.js
+++ src/layout/publicLayout.js
@@ -1,7 +1,18 @@
-import React from "react";
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
 
-function PublicLayout({ children }) {
-  return <div>{children}</div>;
+export default function PublicLayout({ children }) {
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <Grid container spacing={2}>
+        <Grid item xs={7}>
+          <h2>Some placeholder</h2>
+        </Grid>
+        <Grid item xs={5}>
+          {children}
+        </Grid>
+      </Grid>
+    </Box>
+  );
 }
-
-export default PublicLayout;
