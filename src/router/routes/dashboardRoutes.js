40b669fa54c58ed0189531a8fb8ad336b53e8919
Ravi K <ravi.maurya@nickelfox.com>
2022-01-06 13:47:12 +0530

    feat: add routing for sidemenu

---
+++ src/router/routes/dashboardRoutes.js
@@ -0,0 +1,18 @@
+// Export all routes that should be in the side menu
+import React from "react";
+import HomeIcon from "@mui/icons-material/Dashboard";
+import SettingsIcon from "@mui/icons-material/Settings";
+
+class MenuPath {
+  constructor(title, icon, route, alias = null) {
+    this.title = title;
+    this.icon = icon;
+    this.route = route;
+    this.alias = alias || title.replace(" ", "_").toLowerCase();
+  }
+}
+
+export const DashboardMenus = [
+  new MenuPath("Dahboard", <HomeIcon />, "/u/dashboard"),
+  new MenuPath("Settings", <SettingsIcon />, "/u/settings")
+];
